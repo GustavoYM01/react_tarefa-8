f11c10a2366bc9eadccf9bc9c2cc83d90f7d5eb3
GustavoYM01 <gumatimoto@hotmail.com>
2022-07-18 14:13:19 -0300

    git_context

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,22 @@
-const About = () => {
-  return (
-    <div>About</div>
-  )
-}
+// import { useContext } from "react";
+// import { CounterContext } from "../context/CounterContext";
 
-export default About
+import { useCounterContext } from "../hooks/useCounterContext";
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
+
+const About = () => {
+  // const { counter } = useContext(CounterContext);
+  const { counter } = useCounterContext();
+
+  // 5 - context mais complexo
+  const { color } = useTitleColorContext();
+
+  return (
+    <div>
+      <h1 style={{ color: color }}>Sobre</h1>
+      <p>Valor do contador: {counter}</p>
+    </div>
+  );
+};
+
+export default About;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,38 @@
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+// import { useContext } from "react";
+import ChangeCounter from "../components/ChangeCounter";
+// import { CounterContext } from "../context/CounterContext";
 
-export default Home
+// 4 - refatorando com hook
+import { useCounterContext } from "../hooks/useCounterContext";
+
+// 5 - context mais complexo
+import { useTitleColorContext } from "../hooks/useTitleColorContext";
+
+const Home = () => {
+  // const { counter } = useContext(CounterContext);
+  const { counter } = useCounterContext();
+
+  // 5 - context mais complexo
+  const { color, dispatch } = useTitleColorContext();
+
+  // 6 - alterando contexto complexo
+  const setTitleColor = (color) => {
+    dispatch({ type: color });
+  };
+
+  return (
+    <div>
+      <h1 style={{ color: color }}>Home</h1>
+      <p>Valor do contador: {counter}</p>
+      {/* 3 - alterando o valor do contexto */}
+      <ChangeCounter />
+      {/* 6 - alterando contexto complexo */}
+      <div>
+        <button onClick={() => setTitleColor("RED")}>Vermelho</button>
+        <button onClick={() => setTitleColor("BLUE")}>Azul</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
